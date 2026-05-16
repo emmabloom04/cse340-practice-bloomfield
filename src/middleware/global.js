6621aa4bcac3e5a75b281132f6715861c53adcc4
emmabloom04 <blo23006@byui.edu>
2026-05-15 21:38:31 -0600

    adding all local variables for middleware in src/middleware/global.js

--- src/middleware/global.js
+++ src/middleware/global.js
@@ -11,3 +11,24 @@ const getCurrentGreeting = () => {
             return 'Good evening!';
         }
 }
+
+const addLocalVariables = (req, res, next) => {
+    // set current year for use in templates
+    res.locals.currentYear = new Date().getFullYear();
+
+    // make NODE_ENV available to all templates
+    res.locals.NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
+
+    // make req.query available to all templates
+    res.locals.queryParams = { ...req.query };
+
+    // set greeting based on time of day
+    res.locals.greeting = `<p>${getCurrentGreeting}</p>`
+
+    const themes = ['blue-theme', 'green-theme', 'red-theme', 'purple-theme', 'yellow-theme', 'dark-theme'];
+
+    const randomTheme = themes[Math.floor(Math.random() * themes.length)];
+    res.locals.bodyClass = randomTheme;
+
+    next();
+}
