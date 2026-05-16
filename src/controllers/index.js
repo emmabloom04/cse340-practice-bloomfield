cec47c0cfd7e62a57278657dc9d72d1d7770da09
emmabloom04 <blo23006@byui.edu>
2026-05-15 21:55:14 -0600

    adding test 500 error page

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -11,3 +11,9 @@ const aboutPage = (req, res) => {
 const demoPage = (req, res) => {
     res.render('demo', { title: 'Middleware Demo Page' });
 };
+
+const testErrorPage = (req, res, next) => {
+    const err = new Error('This is a test error');
+    err.status = 500;
+    next(err);
+}
