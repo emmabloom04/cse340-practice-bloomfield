921c695ac15dd34e929265fa74f92461763b6fc9
emmabloom04 <blo23006@byui.edu>
2026-05-15 21:57:05 -0600

    adding base code for src/controllers/routes.js

---
+++ src/controllers/routes.js
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+
+//create a new router instance
+const router = Router();
+
+// TODO: add import statements for controllers and middleware
+
+// TODO: add route definitions
+
+export default router;
