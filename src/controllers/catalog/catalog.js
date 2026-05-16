864456fd24df30efa284b34a34a5dd43c515912d
emmabloom04 <blo23006@byui.edu>
2026-05-15 21:44:46 -0600

    creating catalogPage route handler in src/controllers/catalog/catalog.js

--- src/controllers/catalog/catalog.js
+++ src/controllers/catalog/catalog.js
@@ -1,2 +1,11 @@
 import { getAllCourses, getCourseById, getSortedSections } from '../../models/catalog/catalog.js'
 
+// route handler for the course catalog list page
+const catalogPage = (req, res) => {
+    const courses = getAllCourses();
+
+    res.render('catalog', {
+        title: 'Course Catalog',
+        courses: courses
+    });
+};
